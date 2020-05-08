c19054e13f1260e34d62afd7693a940e87f0bcbd
mmcdevitt1997 <30372557+mmcdevitt1997@users.noreply.github.com>
2020-05-08 07:22:54 -0500

    done with series section

--- series.py
+++ series.py
@@ -6,5 +6,15 @@ my_list = [10,20,30]
 arr = np.array([10,20,30])
 d = {'a' :10, 'b':20, 'c':30}
 
-series = pd.Series(my_list)
+series = pd.Series(my_list, index=labels)
+
+# can hold any data type
+labeldata = pd.Series(data=labels)
+
+ser1 = pd.Series([1,2,3,4],index=['USA', 'CHINA','FRANCE', 'GERMANY'])
+ser2 = pd.Series([1,2,3,4],index=['USA', 'CHINA','ITALY', 'JAPAN'])
+
+print(ser1['USA'])
+
+
 
