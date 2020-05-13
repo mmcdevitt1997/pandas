78179b3dbacba850a464414a19a2b3375a546181
mmcdevitt1997 <30372557+mmcdevitt1997@users.noreply.github.com>
2020-05-12 19:18:26 -0500

    done with this one

--- groupby.py
+++ groupby.py
@@ -1,4 +1,12 @@
 import numpy as np
 import pandas as pd
 
-data = {'Company':['GOOG','GOOG',]}
+data = {'Company':['GOOG','GOOG','MSFT','MSFT','FB','FB'],
+        'Person':['Sam','Charlie','Amy', 'Vanessa','Carl','Sarah'],
+        'Sales':[200,120,340,124,243,350]}
+
+df = pd.DataFrame(data)
+
+byComp = df.groupby('Company')
+
+print(byComp.sum())
